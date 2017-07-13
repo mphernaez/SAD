766ac25ef7343b4f0f95631b274db8b695f78ba6
jjpfernandez <jjpfernandez@JF610B-020>
2017-07-13 10:48:08 +0800

    home

--- WindowsFormsApplication1/Properties/Resources.Designer.cs
+++ WindowsFormsApplication1/Properties/Resources.Designer.cs
@@ -133,6 +133,16 @@ namespace WindowsFormsApplication1.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap back {
+            get {
+                object obj = ResourceManager.GetObject("back", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap bg {
             get {
                 object obj = ResourceManager.GetObject("bg", resourceCulture);
