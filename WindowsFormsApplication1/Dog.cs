e0c3d4424f3af263276524ed05a9be75d966724f
mphernaez <mphernaez@JF610B-020>
2017-08-08 11:20:37 +0800

    last - report

--- WindowsFormsApplication1/Dog.cs
+++ WindowsFormsApplication1/Dog.cs
@@ -544,7 +544,9 @@ namespace WindowsFormsApplication1
 
             xlWorkBook = xlApp.Workbooks.Open("dogExcel", 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
             xlApp.Visible = true;
-              
+
+            xlWorkBook.Close();
+
 
 
         }
